c89f660e2744315a534fc17b3075918411df6f9a
jtrtj <jt.r@live.com>
2019-01-09 14:12:20 -0700

    Add usuage and write test for PathFinder #define_trajectory

--- lib/path_finder.rb
+++ lib/path_finder.rb
@@ -2,4 +2,8 @@ class PathFinder
   def initialize(dungeon_map)
     @dungeon_map = dungeon_map
   end
+
+  def moves
+    trajectory = define_trajectory
+  end
 end
--- spec/path_finder_spec.rb
+++ spec/path_finder_spec.rb
@@ -23,6 +23,7 @@ describe PathFinder do
 
     @map_1 = DungeonMap.new(@grid_1)
     @map_2 = DungeonMap.new(@grid_2)
+    @path_finder = PathFinder.new(@map_2)
   end
 
   context 'instance variables' do
@@ -30,6 +31,20 @@ describe PathFinder do
       path_finder = PathFinder.new(@map_1)
 
       expect(path_finder.instance_variable_get(:@dungeon_map)).to eq(@map_1)
+    end
+  end
+
+  context 'instance methods' do
+    it '#moves - returns a list of moves required for mario to reach peach' do
+      expected_moves = ["UP","UP","UP","UP","LEFT","LEFT","LEFT"]
+
+      expect(@path_finder.moves).to eq(expected_moves)
+    end
+
+    it "#define_trajectory - returns the difference between mario and peach's coordiantes" do
+      expected_trajectory = [4,3]
+
+      expect(@path_finder.define_trajectory).to eq(expected_trajectory)
     end
   end
 end
