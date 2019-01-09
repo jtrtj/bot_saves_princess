e3610a2482b9719b2b8594ce07fb3ee7e59a0ce3
jtrtj <jt.r@live.com>
2019-01-09 11:12:01 -0700

    Write first test using example from challenger spec

---
+++ spec/path_to_princess_spec.rb
@@ -0,0 +1,16 @@
+describe SavePrincess do
+  context 'instance methods' do
+    it '#displayPathtoPrincess(n, grid) - returns the directional moves required to rescue the princess' do
+      save_princess = SavePrincess.new
+      n = 3
+      grid = [
+        '---',
+        '-m-',
+        'p--'
+      ]
+      expected = ['DOWN', 'LEFT']
+
+      expect(save_princess.displayPathtoPrincess(n, grid)).to eq(expected)
+    end
+  end
+end
