6d14b27c35b626ed7e1e4cae4eb6dc6437f985ad
jtrtj <jt.r@live.com>
2019-01-09 11:43:57 -0700

    Write test for #character_coordinates on DungeonMap

--- spec/dungeon_map_spec.rb
+++ spec/dungeon_map_spec.rb
@@ -13,4 +13,20 @@ describe DungeonMap do
       expect(map.instance_variable_get(:@grid)).to eq(grid)
     end
   end
+
+  context 'instance methods' do
+    it '#character_coordinates(character_symbol) - returns grid matrix coordiantes of character on map according to provided character symbol' do
+      grid = [
+        'm--',
+        '---',
+        '--p'
+      ]
+      map = DungeonMap.new(grid)
+      expected_m_location = [0,0]
+      expected_p_location = [2,2]
+
+      expect(map.character_coordinates("m")).to eq(expected_m_location)
+      expect(map.character_coordinates("p")).to eq(expected_p_location)
+    end
+  end
 end
