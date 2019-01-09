de536f6dd6063c1b9a9a12ff69a3e362c5931fbb
jtrtj <jt.r@live.com>
2019-01-09 14:15:19 -0700

    Write implementation and pass PathFinder #define_trajectory test

--- lib/path_finder.rb
+++ lib/path_finder.rb
@@ -6,4 +6,10 @@ class PathFinder
   def moves
     trajectory = define_trajectory
   end
+
+  def define_trajectory
+    mario_coordinates = @dungeon_map.character_coordinates("m")
+    princess_coordinates = @dungeon_map.character_coordinates("p")
+    [(mario_coordinates.first - princess_coordinates.first), (mario_coordinates.last - princess_coordinates.last)]
+  end
 end
