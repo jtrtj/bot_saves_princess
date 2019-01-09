27bcce639faa027ffa87c0ab3ad058e4419b06f3
jtrtj <jt.r@live.com>
2019-01-09 13:32:49 -0700

    Refactor DungeonMap to use a before(:all) set up for grid data

--- spec/dungeon_map_spec.rb
+++ spec/dungeon_map_spec.rb
@@ -1,35 +1,51 @@
 require './lib/dungeon_map.rb'
 
 describe DungeonMap do
+  before(:all) do
+    @grid_1 = [
+      '---',
+      '-m-',
+      'p--'
+    ]
+    @expected_m_coordinates_1 = [1,1] 
+    @expected_p_coordinates_1 = [2,0] 
+
+    @grid_2 = [
+      '----------',
+      '----p-----',
+      '----------',
+      '----------',
+      '----------',
+      '-------m--',
+      '----------',
+      '----------',
+      '----------',
+      '----------'
+    ]
+    @expected_m_coordinates_2 = [5,7] 
+    @expected_p_coordinates_2 = [1,4] 
+  end
+
   context 'instance variables' do
     it 'starts with a grid' do
-      grid = [
-        '---',
-        '-m-',
-        'p--'
-      ]
-      map = DungeonMap.new(grid)
+      map = DungeonMap.new(@grid_1)
 
-      expect(map.instance_variable_get(:@grid)).to eq(grid)
+      expect(map.instance_variable_get(:@grid)).to eq(@grid_1)
     end
   end
 
   context 'instance methods' do
     it '#character_coordinates(character_symbol) - returns grid matrix coordiantes of character on map according to provided character symbol' do
-      grid = [
-        'm--',
-        '---',
-        '--p'
-      ]
-      map = DungeonMap.new(grid)
-      expected_m_location = [0,0]
-      expected_p_location = [2,2]
+      map_1 = DungeonMap.new(@grid_1)
+      map_2 = DungeonMap.new(@grid_2)
 
-      expect(map.character_coordinates('m')).to eq(expected_m_location)
-      expect(map.character_coordinates('p')).to eq(expected_p_location)
+      expect(map_1.character_coordinates('m')).to eq(@expected_m_coordinates_1)
+      expect(map_1.character_coordinates('p')).to eq(@expected_p_coordinates_1)
+      expect(map_2.character_coordinates('m')).to eq(@expected_m_coordinates_2)
+      expect(map_2.character_coordinates('p')).to eq(@expected_p_coordinates_2)
     end
 
-    it '#find_row(character_symbol) - returns the character location row index' do
+    it '#find_row(character_symbol) - returns the row specified character is located in' do
       grid = [
         'm--',
         '---',
@@ -43,32 +59,18 @@ describe DungeonMap do
       expect(map.find_row('p')).to eq(expected_p_row)
     end
 
-    it '#find_row_coordinate(character_symbol) - returns grid matrix coordinae for character location row' do
-      grid = [
-        'm--',
-        '---',
-        '--p'
-      ]
-      map = DungeonMap.new(grid)
-      expected_m_row_coordinate = 0
-      expected_p_row_coordinate = 2
+    it '#find_row_coordinate(character_symbol) - returns grid matrix coordinate for character location row' do
+      map = DungeonMap.new(@grid_1)
 
-      expect(map.find_row_coordinate('m')).to eq(expected_m_row_coordinate)
-      expect(map.find_row_coordinate('p')).to eq(expected_p_row_coordinate)
+      expect(map.find_row_coordinate('m')).to eq(@expected_m_coordinates_1.first)
+      expect(map.find_row_coordinate('p')).to eq(@expected_p_coordinates_1.first)
     end
 
-    it '#find_column_coordinate(character_symbol) - returns grid matrix coordinae for character location column' do
-      grid = [
-        'm--',
-        '---',
-        '--p'
-      ]
-      map = DungeonMap.new(grid)
-      expected_m_column_coordinate = 0
-      expected_p_column_coordinate = 2
+    it '#find_column_coordinate(character_symbol) - returns grid matrix coordinate for character location column' do
+      map = DungeonMap.new(@grid_1)
 
-      expect(map.find_column_coordinate('m')).to eq(expected_m_column_coordinate)
-      expect(map.find_column_coordinate('p')).to eq(expected_p_column_coordinate)
+      expect(map.find_column_coordinate('m')).to eq(@expected_m_coordinates_1.last)
+      expect(map.find_column_coordinate('p')).to eq(@expected_p_coordinates_1.last)
     end
   end
 end
