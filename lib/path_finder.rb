cd72d5c154ea0c3062e519913e43d2a5c0f2e12f
jtrtj <jt.r@live.com>
2019-01-09 20:09:26 -0700

    Breakout PathFinder #establish_trajectory into private methods

--- lib/path_finder.rb
+++ lib/path_finder.rb
@@ -10,10 +10,24 @@ class PathFinder
   end
 
   def establish_trajectory
-    mario_coordinates = @dungeon_map.character_coordinates('m')
-    princess_coordinates = @dungeon_map.character_coordinates('p')
-    row_index_difference = mario_coordinates.row_index - princess_coordinates.row_index
-    column_index_difference = mario_coordinates.column_index - princess_coordinates.column_index
     Trajectory.new(row_index_difference, column_index_difference)
+  end
+
+  private
+
+  def row_index_difference
+    mario_coordinates.row_index - princess_coordinates.row_index
+  end
+
+  def column_index_difference
+    mario_coordinates.column_index - princess_coordinates.column_index
+  end
+
+  def mario_coordinates
+    @mario_coordinates ||= @dungeon_map.character_coordinates('m')
+  end
+
+  def princess_coordinates
+    @princess_coordinates ||= @dungeon_map.character_coordinates('p')
   end
 end
