9761472cfe96ad70dc7c2e5b2a243964747e4bda
jtrtj <jt.r@live.com>
2019-01-11 15:17:16 -0700

    Add next_move_grids to GridExamples and use them in SavePrincess test

--- spec/path_to_princess_spec.rb
+++ spec/path_to_princess_spec.rb
@@ -9,33 +9,28 @@ describe SavePrincess do
       grid_examples = GridExamples.new
       @sml_grids = grid_examples.sml_grids
       @med_grids = grid_examples.med_grids
+      @next_move_grids = grid_examples.next_move_grids
+
+      @save_princess = SavePrincess.new
     end
 
     it '#displayPathtoPrincess(grid) - returns the directional moves required to rescue the princess' do
-      save_princess = SavePrincess.new
+      expect(@save_princess.displayPathtoPrincess(@sml_grids[:grid_1])).to eq(@sml_grids[:grid_1_solution])
+      expect(@save_princess.displayPathtoPrincess(@sml_grids[:grid_2])).to eq(@sml_grids[:grid_2_solution])
+      expect(@save_princess.displayPathtoPrincess(@sml_grids[:grid_3])).to eq(@sml_grids[:grid_3_solution])
+      expect(@save_princess.displayPathtoPrincess(@sml_grids[:grid_4])).to eq(@sml_grids[:grid_4_solution])
 
-      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_1])).to eq(@sml_grids[:grid_1_solution])
-      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_2])).to eq(@sml_grids[:grid_2_solution])
-      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_3])).to eq(@sml_grids[:grid_3_solution])
-      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_4])).to eq(@sml_grids[:grid_4_solution])
-
-      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_1])).to eq(@med_grids[:grid_1_solution])
-      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_2])).to eq(@med_grids[:grid_2_solution])
-      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_3])).to eq(@med_grids[:grid_3_solution])
-      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_4])).to eq(@med_grids[:grid_4_solution])
+      expect(@save_princess.displayPathtoPrincess(@med_grids[:grid_1])).to eq(@med_grids[:grid_1_solution])
+      expect(@save_princess.displayPathtoPrincess(@med_grids[:grid_2])).to eq(@med_grids[:grid_2_solution])
+      expect(@save_princess.displayPathtoPrincess(@med_grids[:grid_3])).to eq(@med_grids[:grid_3_solution])
+      expect(@save_princess.displayPathtoPrincess(@med_grids[:grid_4])).to eq(@med_grids[:grid_4_solution])
     end
 
     it '#nextMove(grid) - returns the next directional move to rescue the princess' do
-      grid = [
-        '-----',
-        '-----',
-        'p--m-',
-        '-----',
-        '-----'
-      ]
-      save_princess = SavePrincess.new
-      
-      expect(save_princess.nextMove(grid)).to eq('LEFT')
+      expect(@save_princess.nextMove(@next_move_grids[:grid_1])).to eq(@next_move_grids[:grid_1_solution])
+      expect(@save_princess.nextMove(@next_move_grids[:grid_2])).to eq(@next_move_grids[:grid_2_solution])
+      expect(@save_princess.nextMove(@next_move_grids[:grid_3])).to eq(@next_move_grids[:grid_3_solution])
+      expect(@save_princess.nextMove(@next_move_grids[:grid_4])).to eq(@next_move_grids[:grid_4_solution]) 
     end
   end
 end
