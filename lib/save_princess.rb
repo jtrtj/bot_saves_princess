09282ad2be40e9de71ea93539e1138e943eb3883
jtrtj <jt.r@live.com>
2019-01-11 15:01:53 -0700

    Remove unused n, r and c arguments in SavePrincess instance methods

--- lib/save_princess.rb
+++ lib/save_princess.rb
@@ -1,12 +1,12 @@
 class SavePrincess
-  def displayPathtoPrincess(_n, grid)
+  def displayPathtoPrincess(grid)
     map = DungeonMap.new(grid)
     path_finder = PathFinder.new(map)
     path_finder.moves
   end
 
-  def nextMove(n, r, c, grid)
-    moves = displayPathtoPrincess(n, grid)
+  def nextMove(grid)
+    moves = displayPathtoPrincess(grid)
     moves.first
   end
 end
--- spec/path_to_princess_spec.rb
+++ spec/path_to_princess_spec.rb
@@ -13,23 +13,19 @@ describe SavePrincess do
 
     it '#displayPathtoPrincess(n, grid) - returns the directional moves required to rescue the princess' do
       save_princess = SavePrincess.new
-      n = 3
 
-      expect(save_princess.displayPathtoPrincess(n, @sml_grids[:grid_1])).to eq(@sml_grids[:grid_1_solution])
-      expect(save_princess.displayPathtoPrincess(n, @sml_grids[:grid_2])).to eq(@sml_grids[:grid_2_solution])
-      expect(save_princess.displayPathtoPrincess(n, @sml_grids[:grid_3])).to eq(@sml_grids[:grid_3_solution])
-      expect(save_princess.displayPathtoPrincess(n, @sml_grids[:grid_4])).to eq(@sml_grids[:grid_4_solution])
+      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_1])).to eq(@sml_grids[:grid_1_solution])
+      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_2])).to eq(@sml_grids[:grid_2_solution])
+      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_3])).to eq(@sml_grids[:grid_3_solution])
+      expect(save_princess.displayPathtoPrincess(@sml_grids[:grid_4])).to eq(@sml_grids[:grid_4_solution])
 
-      expect(save_princess.displayPathtoPrincess(n, @med_grids[:grid_1])).to eq(@med_grids[:grid_1_solution])
-      expect(save_princess.displayPathtoPrincess(n, @med_grids[:grid_2])).to eq(@med_grids[:grid_2_solution])
-      expect(save_princess.displayPathtoPrincess(n, @med_grids[:grid_3])).to eq(@med_grids[:grid_3_solution])
-      expect(save_princess.displayPathtoPrincess(n, @med_grids[:grid_4])).to eq(@med_grids[:grid_4_solution])
+      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_1])).to eq(@med_grids[:grid_1_solution])
+      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_2])).to eq(@med_grids[:grid_2_solution])
+      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_3])).to eq(@med_grids[:grid_3_solution])
+      expect(save_princess.displayPathtoPrincess(@med_grids[:grid_4])).to eq(@med_grids[:grid_4_solution])
     end
 
     it '#nextMove(n, r, c, grid) - returns the next directional move to rescue the princess' do
-      n = 5
-      r = 2
-      c = 3
       grid = [
         '-----',
         '-----',
@@ -39,7 +35,7 @@ describe SavePrincess do
       ]
       save_princess = SavePrincess.new
       
-      expect(save_princess.nextMove(n, r, c, grid)).to eq('LEFT')
+      expect(save_princess.nextMove(grid)).to eq('LEFT')
     end
   end
 end
